a250fa6745cf4e7f89cb5222c14b745232f074f4
Anita <anitajn7990@gmail.com>
2024-02-19 10:46:17 +0300

    developing the page

--- WEEK6/script.js
+++ WEEK6/script.js
@@ -1,3 +1,24 @@
 console.log("Hello");
 
+//window.alert('Hey guys');
+//alert("How are you");
 
+if (confirm("Press a button!")){
+    txt="You pressed OK";
+} else{
+    txt="You pressed cancel";
+}
+
+var userInput=window.prompt("Please enter your name",);
+if (userInput!==null) {
+    alert("Hello,"+userInput+"!");
+}else{
+    alert("You cancelled the prompt.");
+}
+
+function openPrompt(){
+    var inputValue=prompt("Please just type something","");
+    if (inputValue !==null){
+        alert("Thank you for your contribution"+inputValue);
+    }
+}
